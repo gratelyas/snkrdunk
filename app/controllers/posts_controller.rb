4145d538aad8281256cda96bf585405409423b1a
gratelyas <gratel.yas@gmail.com>
2020-01-21 14:29:17 +0900

    commit-text-ok

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,5 +4,20 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new 
+    @post = Post.new
+  end
+
+
+  def create
+    Post.create(post_params)
+    redirect_to root_path
+  end
+
+
+  def post_params
+    params.require(:post).permit(:title, :subtitle, :content)
+  end
+
 
 end
