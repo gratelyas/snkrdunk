100bcf18c79dbf3af3b03e1e67a761bd06d98eb4
gratelyas <gratel.yas@gmail.com>
2020-01-21 17:26:11 +0900

    画像投稿OK

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,8 +15,31 @@ class PostsController < ApplicationController
   end
 
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path
+  end
+
+
+  private
   def post_params
-    params.require(:post).permit(:title, :subtitle, :content)
+    params.require(:post).permit(:title, :image, :subtitle, :content)
   end
 
 
